3621bdb41426a5ba1e37f819ac32ccea67452e20
geirmj11 <geirmj11@ru.is>
2013-01-21 21:44:25 +0000

    Agent can now go home without running in circles

--- src/AwesomeAgent.java
+++ src/AwesomeAgent.java
@@ -5,28 +5,27 @@ public class AwesomeAgent implements Agent
 {
 	final String BUMP = "BUMP";
 	final String DIRT = "DIRT";
-	
+
 	final String TURN_LEFT = "TURN_LEFT";
 	final String TURN_RIGHT = "TURN_RIGHT";
 	final String TURN_OFF = "TURN_OFF";
 	final String SUCK = "SUCK";
 	final String GO = "GO";
-	
-	
-	int faceing;
+
+	int orientation;
 	int StartX;
 	int StartY;
-	
+
 	int State = 0;
-	
+
 	int TurnState = 0;
 	String lastTurn;
-	
+
 	int DisEastWall = -1;
 	int DisWestWall = -1;
-	
+
     public String nextAction(Collection<String> percepts) {
-		System.out.print("STATE = "+State+", Faceing = "+ faceing +"\n");
+		System.out.print("STATE = "+State+", orientation = "+ orientation +"\n");
 		String command = GO;
 		String percept = "";
 		for (String p : percepts) {
@@ -35,46 +34,46 @@ public class AwesomeAgent implements Agent
 			if (percept.equals(DIRT))
 				return SUCK;
 		}
-		
+
 		switch (State)
 		{
 			case 0: // Find north wall ! :)
 			command = FindNorthLeftCorner(percept);
 			break;
-			
+
 			case 1: // Sweep the area :D
 			command = Sweep(percept);
 			break;
-			
-			case 2: // Go home ! 			
+
+			case 2: // Go home ! 
 			command = GoHome(percept);
 			break;
 		}
-		
+
 		trackPosition(command);
 		return command;
 	}
-	
+
 	String FindNorthLeftCorner(String percept)
 	{
 		if (percept.equals(BUMP))
-			if (faceing == 0 || faceing == 3)
+			if (orientation == 0 || orientation == 3)
 				return TURN_LEFT;
-			
-		if (faceing == 2){
+
+		if (orientation == 2){
 			State = 1;
 			return TURN_LEFT;
 		}
 		return GO;
 	}
-	
+
 	String Sweep(String percept)
 	{
 		if (percept.equals("") && TurnState == 0)
 			return GO;
 		if (percept.equals(BUMP) && TurnState == 0) {
 			TurnState = 1;
-			lastTurn = faceing == 1 ? TURN_RIGHT : TURN_LEFT;
+			lastTurn = orientation == 1 ? TURN_RIGHT : TURN_LEFT;
 			return lastTurn;
 		}
 		if (TurnState == 1) {
@@ -88,54 +87,63 @@ public class AwesomeAgent implements Agent
 		if (TurnState == 2) {
 			TurnState = 0;
 			return lastTurn;
-		}		
-		
+		}
+
 		return GO;
 	}
-	
+
 	String GoHome(String percept)
 	{
-		return GO; // TODO.
+	    if(StartX == 0 && StartY == 0)
+	        return TURN_OFF;
+	    if(StartY > 0 && orientation == 2)
+	        return GO;
+	    if(StartY > 0 && orientation != 2)
+	        return lastTurn;
+	    if(StartX < 0 && orientation != 3)
+	        return lastTurn;
+	    if(StartX > 0 && orientation != 1)
+	        return lastTurn;
+	    return GO; 
 	}
-	
+
 	void trackPosition(String command)
 	{
-		System.out.print("Command from TrackPosistion: " + command + "\n");
+		System.out.print("Command from TrackPosistion: " + command + "\n" + "StartX: " + StartX + "\nStartY: " + StartY + "\n");
 		if (command.equals(TURN_LEFT))
-			faceing = (faceing + 3) % 4;
+			orientation = (orientation + 3) % 4;
 		if (command.equals(TURN_RIGHT))
-			faceing = (faceing + 1) % 4;
+			orientation = (orientation + 1) % 4;
 		if (command.equals(GO))
 		{
-			switch (faceing)
+			switch (orientation)
 			{
 				case 0: // North
 					StartY++;
 					break;
-				
+
 				case 1: // East
 					if (DisEastWall >= 0)
 						DisEastWall--;
 					if (DisWestWall >= 0)
 						DisWestWall++;
-						
+
 					StartX--;
 					break;
-				
+
 				case 2: // South
 					StartY--;
 					break;
-				
+
 				case 3: // West
 					if (DisWestWall >= 0)
 						DisWestWall--;
 					if (DisEastWall >= 0)
 						DisEastWall++;
-						
+
 					StartX++;
-					break;	
+					break;
 			}
 		}
 	}
-	
 }
