a055873ecea28ad5c7fd8038a3241a80dae3843e
Þorgeir Auðunn Karlsson <thorgeirk11@ru.is>
2013-01-22 17:53:27 +0000

    Final solution !

--- src/AwesomeAgent.java
+++ src/AwesomeAgent.java
@@ -12,13 +12,13 @@ public class AwesomeAgent implements Agent
 	final String SUCK = "SUCK";
 	final String GO = "GO";
     
-	int orientation;
-	int StartX;
-	int StartY;
+	int orientation; // 0 = North, 1 = East, 2 = South, 3 = West.
+	int startX;
+	int startY;
 
-	int State = 0;
+	int state = 0;
 
-	int TurnState = 0;
+	int turnState = 0;
 	String lastTurn;
 
 	int DisEastWall = -1;
@@ -33,9 +33,9 @@ public class AwesomeAgent implements Agent
 				return SUCK;
 		}
 
-		switch (State)
+		switch (state)
 		{
-			case 0: // Find north wall ! :)
+			case 0: // Find north left corner ! :)
 			command = FindNorthLeftCorner(percept);
 			break;
 
@@ -56,16 +56,16 @@ public class AwesomeAgent implements Agent
 	{
 		if (percept.equals(BUMP)){
 			if (orientation == 0){
-			    StartY--;
+			    startY--;
 				return TURN_LEFT;
 			}
 
 		    if (orientation == 3){
-			    State = 1;
-			    StartX--;
-			    StartY--;
+			    state = 1;
+			    startX--;
+			    startY--;
 			    DisWestWall = 0;
-			    TurnState = 2;
+			    turnState = 2;
 			    lastTurn = TURN_LEFT;
 			    return lastTurn;
 		    }
@@ -76,43 +76,43 @@ public class AwesomeAgent implements Agent
 	String Sweep(String percept)
 	{
         if(lastTurn == TURN_LEFT && DisEastWall == 0){
-            TurnState = 1;
+            turnState = 1;
             lastTurn = TURN_RIGHT;
             return lastTurn;
         }
         if(lastTurn == TURN_RIGHT && DisWestWall == 0){
-            TurnState = 1;
+            turnState = 1;
             lastTurn = TURN_LEFT;
             return lastTurn;
         }
-        if (percept.equals("") && TurnState == 0)
+        if (percept.equals("") && turnState == 0)
 			return GO;
-		if (percept.equals(BUMP) && TurnState == 0) {
-			TurnState = 1;
+		if (percept.equals(BUMP) && turnState == 0) {
+			turnState = 1;
 			if(orientation == 1) {
 			    DisEastWall = 0;
 			    DisWestWall--;  
-			    StartX++; 
+			    startX++; 
 			}
 			if(orientation == 3) {
 	            DisWestWall = 0;
-	            DisEastWall--;    
-	            StartX--;
+	            DisEastWall--;   
+	            startX--;
 	        }
 			lastTurn = orientation == 1 ? TURN_RIGHT : TURN_LEFT;
 			return lastTurn;
 		}
-		if (TurnState == 1) {
-			TurnState = 2;
+		if (turnState == 1) {
+			turnState = 2;
 			return GO;
 		}
-		if (percept.equals(BUMP) && TurnState == 2) {
-			State = 2; // Just hit the last wall.
-			StartY++;
+		if (percept.equals(BUMP) && turnState == 2) {
+			state = 2; // Just hit the last wall.
+			startY++;
 			return lastTurn;
 		}
-		if (TurnState == 2) {
-			TurnState = 0;
+		if (turnState == 2) {
+			turnState = 0;
 			return lastTurn;
 		}
 		return GO;
@@ -120,16 +120,16 @@ public class AwesomeAgent implements Agent
 
 	String GoHome(String percept)
 	{
-	    if(StartX == 0 && StartY == 0)
+	    if(startX == 0 && startY == 0)
 	        return TURN_OFF;
-	    if(StartY < 0 && orientation == 0)
+	    if(startY < 0 && orientation == 0)
 	        return GO;
-	    if(StartY < 0 && orientation != 0)
+	    if(startY < 0 && orientation != 0)
 	        return lastTurn;
 
-	    if(StartX < 0 && orientation == 3)
+	    if(startX < 0 && orientation == 3)
 	        return GO;
-	    if(StartX > 0 && orientation == 1)
+	    if(startX > 0 && orientation == 1)
 	        return GO;
 	    return lastTurn; 
 	}
@@ -145,36 +145,36 @@ public class AwesomeAgent implements Agent
 			switch (orientation)
 			{
 				case 0: // North
-					StartY++;
+					startY++;
 					break;
 
 				case 1: // East
-					if (State == 1)
+					if (state == 1)
 						DisEastWall--;
-					if (State == 1)
+					if (state == 1)
 						DisWestWall++;
 
-					StartX--;
+					startX--;
 					break;
 
 				case 2: // South
-					StartY--;
+					startY--;
 					break;
 
 				case 3: // West
-					if (State == 1){
+					if (state == 1){
 						DisWestWall--;
 				    }
-					if (State == 1){
+					if (state == 1){
 						DisEastWall++;
 					}
-					StartX++;
+					startX++;
 					break;
 			}
 		}
-		//System.out.print("::::::::::::::::\nSTATE = " + State + " \t\t\tCommand: " + command + "--------------------\n");
-		System.out.print("-------------\nSTATE = " + State + ", orientation = "+ orientation + " Command: " + command);
-		System.out.print("\n" + "StartX: " + StartX + "\nStartY: " + StartY);
-		System.out.print("\nWest Wall: " + DisWestWall + "\nEast Wall: " + DisEastWall + "\n:::::::::::::\n");            
+		//System.out.print("::::::::::::::::\nSTATE = " + state + " \t\t\tCommand: " + command + "--------------------\n");
+		//System.out.print("-------------\nSTATE = " + state + ", orientation = "+ orientation + " Command: " + command);
+		//System.out.print("\n" + "startX: " + startX + "\nStartY: " + startY);
+		//System.out.print("\nWest Wall: " + DisWestWall + "\nEast Wall: " + DisEastWall + "\n:::::::::::::\n");            
 	}
 }
--- src/Oriantation.java
+++
@@ -1,7 +0,0 @@
-public enum Oriantation
-{
-	North,
-	West,
-	South,
-	East
-}
